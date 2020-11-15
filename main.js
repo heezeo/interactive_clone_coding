a8335bcab7f7e1cbd6ce49538c80c05dfc7fde7f
김희지 <kimheeji@gimhuijiui-MacBookPro-2.local>
2020-11-16 01:05:59 +0900

    make it act

---
+++ main.js
@@ -0,0 +1,30 @@
+(() => {
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElems = document.querySelectorAll('.graphic-item');
+    let currentItem; //현재 활성화된(visible class가 붙은). graphic-item을 지정
+
+    for(let i = 0; i < stepElems.length; i++) {
+        stepElems[i].CDATA_SECTION_NODE.index = i;
+        graphicElems[i].dataset.index = i;
+    }
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+        
+        for (let i = 0; i < stepElems.length; i++) {
+            step = stepElems[i];
+            boundingRect = step.getBoundingClientRect();
+            
+            if (boundingRect.top > window.innerHeight * 0.1 &&
+                boundingRect.top < window.innerHeight * 0.8) {
+                    if (currentItem) {
+                        currentItem.classList.remove('visible');
+                    }
+                    currentItem = graphicElems[step.dataset.index];
+                    currentItem.classList.add('visible');
+                }
+        }
+    });
+
+})();
