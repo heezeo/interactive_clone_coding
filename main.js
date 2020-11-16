6f18481d2e8d5173cb8744b9825232efb857de78
김희지 <kimheeji@gimhuijiui-MacBookPro-2.local>
2020-11-16 12:54:23 +0900

    20201116 final work

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 (() => {
 
     const actions = {
-        birdFlies() {
-            document.querySelector('[data-index="2"].bird').style.transform = `translateX(${window.innerWidth}px)`;
+        birdFlies(key) {
+            if (key) {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+            }
+        },
+        birdFlies2(key) {
+            if (key) {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+            }
         }
-    }
+    };
 
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
@@ -15,7 +26,7 @@
         ioIndex = entries[0].target.dataset.index * 1;
     });
 
-    for(let i = 0; i < stepElems.length; i++) {
+    for (let i = 0; i < stepElems.length; i++) {
         io.observe(stepElems[i]);
         stepElems[i].dataset.index = i;
         graphicElems[i].dataset.index = i;
@@ -24,14 +35,17 @@
     function activate(action) {
         currentItem.classList.add('visible');
         if (action) {
-            actions[action]();
+            actions[action](true);
         }
     }
 
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible');
+        if (action) {
+            actions[action](false);
+        }    
     }
-    
+
     window.addEventListener('scroll', () => {
         let step;
         let boundingRect;
@@ -41,18 +55,20 @@
             if (!step) continue;
             boundingRect = step.getBoundingClientRect();
             
-            temp++;
-
             if (boundingRect.top > window.innerHeight * 0.1 &&
                 boundingRect.top < window.innerHeight * 0.8) {
 
-                    inactivate();
-                    currentItem = graphicElems[step.dataset.index];
-                    activate(currentItem.dataset.action);
-                }
+                inactivate(currentItem.dataset.action);
+                currentItem = graphicElems[step.dataset.index];
+                activate(currentItem.dataset.action);
             }
+        }
+    });
 
-            console.log(temp);
-        });
-        activate();
+	window.addEventListener('load', () => {
+		setTimeout(() => scrollTo(0, 0), 100);
+	});
+
+	activate();
+
 })();
